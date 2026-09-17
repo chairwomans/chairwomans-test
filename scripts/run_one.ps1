caf497d433debacfcbbd88787777146995935c85
0euun <jeyejong70@naver.com>
2026-09-17 19:49:23 +0900

    data: dataset setting

--- scripts/run_one.ps1
+++ scripts/run_one.ps1
@@ -1,5 +1,6 @@
 param(
-    [Parameter(Mandatory=$true)][string]$DataRoot,
+    # CoOp-style shared data root. The datasets themselves stay outside Git.
+    [string]$DataRoot = "C:\datasets\coop",
     [Parameter(Mandatory=$true)][string]$Dataset,
     [ValidateSet("ViT-B/16", "ViT-B/32", "ViT-L/14")][string]$Backbone = "ViT-B/16",
     [int]$Seed = 42,
